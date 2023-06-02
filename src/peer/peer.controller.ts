8ea097425479418d5f5fda596df5905f7752af91
Wardi <wardi@nusa.net.id>
2023-06-02 08:24:23 +0700

    add advertisement, up and down controller in peer module

--- src/peer/peer.controller.ts
+++ src/peer/peer.controller.ts
@@ -4,4 +4,19 @@ import { PeerService } from './peer.service';
 @Controller('peer')
 export class PeerController {
   constructor(private readonly peerService: PeerService) {}
+
+  @Post('advertisement')
+  async advertise(@Body() { user, upstream, advertisement }) {
+    return this.peerService.advertise(user, upstream, advertisement);
+  }
+
+  @Post('down')
+  async disablePeer(@Body() { user, peer }) {
+    return this.peerService.disablePeer(user, peer);
+  }
+
+  @Post('up')
+  async enablePeer(@Body() { user, peer }) {
+    return this.peerService.enablePeer(user, peer);
+  }
 }
--- src/peer/peer.service.ts
+++ src/peer/peer.service.ts
@@ -1,7 +1,80 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { exec } from 'child_process';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 @Injectable()
 export class PeerService {
   constructor(private configService: ConfigService) {}
+
+  async advertise(user: string, upstream: string[], advertisement: any) {
+    const configFile =
+      this.configService.get('DATA_DIRECTORY') +
+      '/' +
+      this.configService.get('CONFIG_FILE');
+    const data = yaml.dump({ upstream, advertisement });
+    const date = new Date();
+    const now = this.getJakartaFormatedTime(date);
+    const timeStamp = Math.floor(date.getTime() / 1000);
+    const configHeader = [
+      `## ${this.configService.get('CONFIG_PREAMBULE')}`,
+      `## ${this.configService.get('DOC_URL')}`,
+      `## ${now} ${user}`,
+      '',
+    ];
+
+    const content = configHeader.join('\n') + data;
+    const configFileHistory = `${configFile}-${timeStamp}`;
+
+    fs.writeFileSync(configFile, content, 'utf8');
+    fs.writeFileSync(configFileHistory, content, 'utf8');
+    exec(this.configService.get('POST_EXEC'));
+
+    return 'OK';
+  }
+
+  async disablePeer(user: string, peer: string) {
+    const content = this.configService.get('PEER_DISABLE_COMMAND') + ' ' + peer;
+    const file =
+      this.configService.get('DATA_DIRECTORY') +
+      '/' +
+      this.configService.get('PEER_DISABLE_AT_FILE');
+
+    fs.writeFileSync(file, content, 'utf8');
+    exec(this.configService.get('PEER_DISABLE_WRAPPER_COMMAND'));
+
+    return 'OK';
+  }
+
+  async enablePeer(user: string, peer: string) {
+    const content = this.configService.get('PEER_ENABLE_COMMAND') + ' ' + peer;
+    const file =
+      this.configService.get('DATA_DIRECTORY') +
+      '/' +
+      this.configService.get('PEER_ENABLE_AT_FILE');
+
+    fs.writeFileSync(file, content, 'utf8');
+    exec(this.configService.get('PEER_ENABLE_WRAPPER_COMMAND'));
+
+    return 'OK';
+  }
+
+  getJakartaFormatedTime(datetime: Date) {
+    const options: Intl.DateTimeFormatOptions = {
+      timeZone: 'Asia/Jakarta',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false,
+    };
+
+    const formatter = new Intl.DateTimeFormat('en-US', options);
+    const [date, time] = formatter.format(datetime).split(', ');
+    const [mm, dd, yyyy] = date.split('/');
+    return `${yyyy}-${mm}-${dd} ${time}`;
+  }
 }
